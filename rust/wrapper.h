1f9e0ecd35d11cdbe6120d25c2deb93ff512a322
Chris Cowan <agentme49@gmail.com>
2023-12-18 01:55:24 -0800

    fix CI errors

--- rust/wrapper.h
+++ rust/wrapper.h
@@ -1,2 +1,12 @@
+// Some hacks to ignore some headers that don't work with Bindgen on all platforms.
+#if defined(__i386__) || defined(__x86_64__)
+#define __IMMINTRIN_H
+#define __XMMINTRIN_H
+#define __EMMINTRIN_H
+#define __PMMINTRIN_H
+#define SDL_cpuinfo_h_
+#include <mmintrin.h>
+#endif
+
 #include "../Quake/quakedef.h"
 #include "../Quake/cfgfile.h"
