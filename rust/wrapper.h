2faaba1e3591e6259a5d6bd11ec952e196f19e32
Chris Cowan <agentme49@gmail.com>
2026-07-09 00:27:02 -0700

    build fixes
    also remove some old workarounds

--- rust/wrapper.h
+++ rust/wrapper.h
@@ -1,15 +1,4 @@
-// Some hacks to ignore some headers that don't work with Bindgen on all platforms.
-#if defined(__i386__) || defined(__x86_64__)
-#define __IMMINTRIN_H
-#define __XMMINTRIN_H
-#define __EMMINTRIN_H
-#define __PMMINTRIN_H
-#define __FMA4INTRIN_H
-#define __AMMINTRIN_H
-#define __XOPINTRIN_H
-#define SDL_cpuinfo_h_
-#include <mmintrin.h>
-#endif
+#define USE_SDL3
 
 #include "../Quake/quakedef.h"
 #include "../Quake/cfgfile.h"
